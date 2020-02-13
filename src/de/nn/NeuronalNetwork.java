17284ad3d0a2e351a524c43bf28eedbf97132fe0
Rares <rsahleanu@gmail.com>
2020-02-13 18:18:27 +0100

    Initial commit

---
+++ src/de/nn/NeuronalNetwork.java
@@ -0,0 +1,32 @@
+package de.nn;
+
+import java.util.Scanner;
+
+public class NeuronalNetwork {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("PROOF or LEARN");
+        String msg = sc.nextLine();
+        if(msg.equalsIgnoreCase("PROOF")){
+
+        }else{
+            if(msg.equalsIgnoreCase("LEARN")){
+
+            }else{
+                System.exit(1);
+            }
+        }
+    }
+
+
+    public static void learn(){
+        while (true){
+
+            System.out.println("Insert lentgh");
+
+
+        }
+    }
+
+}
